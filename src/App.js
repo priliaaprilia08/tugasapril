83a00d6eaa4f7c8bcd788ee19a7f0599a826f6b5
priliaaprilia08 <apriliapriliarahmawatia@gmail.com>
2022-09-14 09:35:12 +0700

    menambahkan tugas praktikum introduction react

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import "./asset/css/main.css";
 import Home from "./Home";
+import Contact from "./Contact";
 
 function App() {
   return (
     <div>
       <Home />
+      <Contact />
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -4,88 +4,48 @@ import img from "./asset/img/logo-ALTA@2x.png";
 import img2 from "./asset/img/matthew-hamilton-tNCH0sKSZbA-unsplash.jpg";
 
 function Home() {
-  return (
+    return (
     <body class="home-background">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <img class="logo-atas" src={img} />
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div
-            class="collapse navbar-collapse justify-content-end"
-            id="navbarNav"
-          >
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <ul class="nav justify-content-end">
-                  <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">
-                      Activity
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                      Profile
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link disabled">Home</a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <div class="container">
-        <div class="row align-items-center isi-home">
-          <div class="col-lg-4 col-md-12 col-sm-12 col-12">
-            <img id="foto-profil" src={img2} />
-          </div>
-          <div class="col-lg-8 col-md-12 col-sm-12 col-12">
-            <div class="main-p">
-              <p class="sapaan">Welcome</p>
-              <p class="nama">Alterra Academy</p>
-              <p class="hobi">
-                Menghasilkan Talenta IT Berkualitas yang #MasukIndustriIT
-              </p>
-              <p class="tombol">
-                <a href="#">Get Started</a>
-              </p>
+        <header>
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-8 col-md-12 col-sm-12 col-12">
+                        <div class="alterra">
+                            <a href="index.html"><img class="logo-atas" src={img} /></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12 col-sm-12 col-12">
+                        <div class="container">
+                            <div class="row align-items-center navig">
+                                <div class="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-right"><a href="index.html" class="aktif">HOME</a></div>
+                                <div class="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-center"><a href="about.html">ABOUT</a></div>
+                                <div class="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-left"><a href="#">EXPERIENCE</a></div>
+                                <div class="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-right"><a href="form.html">CONTACT</a></div> 
+                            </div>
+                        </div>
+                    </div>    
+                </div>
+            </div> 
+        </header>
+            <div class="container h-100">
+                <div class="row align-items-center isi-home">
+                    <div class="col-lg-4 col-md-12 col-sm-12  col-12">
+                        <img id="foto-profil" src={img2} />
+                    </div>
+                    <div class="col-lg-8 col-md-12 col-sm-12 col-12">
+                        <div class="main-p">
+                            <p class="sapaan">Hi, my name is </p>
+                            <p class="nama">Anne Sullivan</p>
+                            <p class="hobi">I build things for the web</p>
+                            <p class="tombol"><a href="about.html">Get In Touch</a></p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="row align-items-center p-5">
-          <p className="col-sm">
-            &copy;{new Date().getFullYear()} Kampus Merdeka by Aprilia | All rights reserved |
-            Terms Of Service | Education
-          </p>
-        </div>
-          </div>
-        </div>
-      </div>
-      <script
-        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
-        crossorigin="anonymous"
-      ></script>
-      <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
-        crossorigin="anonymous"
-      ></script>
-    </body>
-  );
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>    
+        </body>
+    );
 }
-
+  
 export default Home;
